785618034df0bb6c6588fc97a38d2a0375924d59
Giuliano Zocca <zoccag@gmail.com>
2017-03-21 22:56:00 +0100

    Parte 1

---
+++ ReactSchool/app/dispatcher.js
@@ -0,0 +1,19 @@
+﻿var uuid = require("node-uuid-v4");
+
+var listeners = {};
+
+function dispatch(payload) {
+    for (var id in listeners) {
+        listeners[id](payload);
+    }
+}
+
+function register(cb) {
+    var id = uuid()
+    listeners[id] = cb;
+}
+
+module.exports = {
+    register: register,
+    dispatch: dispatch
+}
