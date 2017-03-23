8ed431188ddeca24556068e81ebcd8294d5ade4c
Giuliano Zocca <zoccag@gmail.com>
2017-03-23 21:49:20 +0100

    <Menu.html> OK il CSS, manca la separazione del .jsx

--- ReactSchool/app/menu.jsx
+++ ReactSchool/app/menu.jsx
@@ -1,17 +1,2 @@
-﻿var App = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <h1>Simple SPA</h1>
-                <ul className="header">
-                    <li>Home</li>
-                    <li>Stuff</li>
-                    <li>Contact</li>
-                </ul>
-                <div className="content">
-
-                </div>
-            </div>
-        )
-    }
-});
+﻿var React = require("react");
+var ReactDOM = require("react-dom");
